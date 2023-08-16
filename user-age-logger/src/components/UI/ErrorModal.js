662191639f221b909b4b95d93464e28f0e494a25
rishij0012 <rajrishij@gmail.com>
2023-08-16 13:02:55 +0530

    react portal added

--- user-age-logger/src/components/UI/ErrorModal.js
+++ user-age-logger/src/components/UI/ErrorModal.js
@@ -1,15 +1,17 @@
 import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
 
 import Cards from "./Cards";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
 function ErrorModal(props) {
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onClick}></div>;
+  };
 
-
-  return (
-    <Fragment>
-      <div className={classes.backdrop} onClick={props.onClick}></div>
+  const Modal = (props) => {
+    return (
       <Cards className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -18,9 +20,24 @@ function ErrorModal(props) {
           <p>{props.message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button onClick={props.onClick} type="button">Close</Button>
+          <Button onClick={props.onClick} type="button">
+            Close
+          </Button>
         </footer>
       </Cards>
+    );
+  };
+
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onClick} />,
+        document.getElementById("backdrop")
+      )}
+      {ReactDOM.createPortal(
+        <Modal onClick={props.onClick} title={props.title} message={props.message}/>,
+        document.getElementById("overlay")
+      )}
     </Fragment>
   );
 }
